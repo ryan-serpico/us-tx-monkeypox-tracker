b2d9753a9a0b1fb3f6d2a57c2677fb31313d0d1f
Ryan Serpico <Ryguy360@gmail.com>
2022-07-26 14:00:03 -0500

    Corrected geckdriver error

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def getPage():
     options = Options()
     options.headless = True
 
-    driver = webdriver.Firefox(service=Service(executable_path='driver/ubuntu/geckodriver   '), options=options)
+    driver = webdriver.Firefox(service=Service(executable_path='driver/ubuntu/geckodriver'), options=options)
     # driver = webdriver.Firefox(service=Service(executable_path='driver/geckodriver'), options=options)
 
     driver.get(monkeypox_website)
